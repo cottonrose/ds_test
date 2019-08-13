260219f41c9c91ab91784fc4dce6cd759898bf19
cottonrose <43376921+cottonrose@users.noreply.github.com>
2019-08-13 22:39:33 +0800

    Update Seqlist.c

--- Seqlist.c
+++ Seqlist.c
@@ -138,18 +138,107 @@ bool DeleteS_T(Seqlist *L,ElemType value,ElemType s,ElemType t){
     L->length = j;
     return true;
 }
+
+//5.从顺序表中删除其值在给定值s与t之间（包含s和t、要求s<t）的所有元素，如果s与t不合理顺序表为空，则显示出错信息并退出运行。
+bool DeleteInS_T(Seqlist *L,ElemType s,ElemType t){
+    if(s>=t||L->length==0){
+        return false;
+    }
+    int cur = 0;
+    for(cur=0;cur<L->length&&L->data[cur]<s;cur++);
+    if(cur>=L->length){
+        return false;
+    }
+    int j = cur;
+    for(;cur<L->length&&L->data[cur]<=t;cur++);
+    if(cur>=L->length){
+        return false;
+    }
+    while(cur<L->length){
+        L->data[j++] = L->data[cur++];
+    }
+    L->length = j;
+    return true;
+}
+//6.从有序顺序表中删除所有其值重复的元素，使表中所有元素的值均不同。
+bool DeleteAllSame(Seqlist *L){
+    if(L->length==0){
+        return false;
+    }
+    int cur = 0;
+    int i = 0;
+    while(cur<L->length){
+        L->data[i] = L->data[cur];
+        if(L->data[cur+1]!=L->data[i]){
+            i++;
+        }
+        cur++;
+    }
+    L->length = i;
+    return true;
+}
+//7.将两个有序顺序表合并为一个新的有序顺序表，并由函数返回结果顺序表。
+bool Merge(Seqlist A,Seqlist B,Seqlist *C
+){
+    if(A.length==0&&B.length==0){
+        return false;
+    }
+    if(A.length+B.length>MAX_SIZE){
+        return false;
+    }
+    int a = 0,b = 0,c = 0;
+    while(a<A.length&&b<B.length){
+        if(A.data[a]<=B.data[b]){
+            C->data[c++] = A.data[a++];
+        }
+        else{
+            C->data[c++] = B.data[b++];
+        }
+    }
+    while(a<A.length){
+        C->data[c++] = A.data[a++];
+    }
+    while(b<B.length){
+        C->data[c++] = B.data[b++];
+    }
+    C->length = c;
+    return true;
+}
+//8.已知在一维数组A[m+n]中依次存放两个线性表（a1,a2,a3,...,am）和（b1,b2,b3,...,bn），试编写一个函数，将数组中两个顺序表的位置互换，即将（b1,b2,b3,...,bn）放在（a1,a2,a3,...,am）的前面。
+
 void main(){
     Seqlist s;
     SeqInit(&s);
     SeqInsert(&s,1,1);
     SeqInsert(&s,2,2);
-    SeqInsert(&s,3,3);
+    SeqInsert(&s,3,2);
     SeqInsert(&s,4,4);
-    SeqInsert(&s,5,5);
+    SeqInsert(&s,5,4);
     SeqInsert(&s,6,6);
     SeqPrint(s);
+    Seqlist a;
+    SeqInit(&a);
+    SeqInsert(&a,1,3);
+    SeqInsert(&a,2,3);
+    SeqInsert(&a,3,5);
+    SeqInsert(&a,4,5);
+    SeqInsert(&a,5,7);
+    SeqInsert(&a,6,9);
+    SeqInsert(&a,7,10);
+    SeqPrint(a);
+    printf("合并两个顺序表之后结果为：");
+    Seqlist c;
+    SeqInit(&c);
+    Merge(s,a,&c);
+    SeqPrint(c);
+    printf("删除所有重复元素之后的顺序表为：");
+    DeleteAllSame(&s);
+    SeqPrint(s);
     printf("删除给定值s-t中的后顺序表为：");
-    DeleteS_T(&s,1,1,5);
+    DeleteS_T(&s,4,1,5);
+    SeqPrint(s);
+    printf("删除给定值s-t(包含s-t)中的后顺序表为：");
+    DeleteInS_T(&s,2,4);
     SeqPrint(s);
     DeleteX(&s,2);
     SeqPrint(s);
@@ -165,6 +254,5 @@ void main(){
     SeqPrint(s);
     
 }
-
 
 
